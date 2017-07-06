bad834fcec2e48cd71f3f5ee12fa0a4c7298b82d
Danni Liu <dannielleorlando@gmail.com>
2017-07-06 11:29:21 -0400

    o

--- react-router-workshop/browser/react/components/SingleArtist.js
+++ react-router-workshop/browser/react/components/SingleArtist.js
@@ -2,8 +2,7 @@ import React, { Component } from 'react';
 import Songs from '../components/Songs';
 import axios from 'axios';
 import AllAlbums from './AllAlbums';
-import { Link } from 'react-router-dom';
-import { HashRouter, Route } from 'react-router-dom';
+import { HashRouter, Route, Link } from 'react-router-dom';
 
 export default class SingleArtist extends Component {
   constructor () {
@@ -36,15 +35,16 @@ export default class SingleArtist extends Component {
     console.log('albums', albums);
 
     return (
+    <HashRouter>
     <div>
       <h3>{ artist.name }</h3>
       <ul className="nav nav-tabs">
         <li><Link to={`/artists/${artist.id}/albums`}>ALBUMS</Link></li>
         <li><Link to={`/artists/${artist.id}/songs`}>SONGS</Link></li>
       </ul>
-       <Route exact path="/artists/:artistId/albums" render={(routeProps) => <AllAlbums albums={albums} />} />
-       <Route exact path="/artists/:artistId/songs" render={(routeProps) => <Songs songs={songs} />} />
-
-    </div>);
+       <Route exact path="/artists/:artistId/albums" render={() => <AllAlbums albums={albums} />} />
+       <Route exact path="/artists/:artistId/songs" render={() => <Songs songs={songs} />} />
+    </div>
+    </HashRouter>);
   }
 }
